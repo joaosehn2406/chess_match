143306b706f6052ef54fbc4ff1942855dd85880b
joaosehn2406 <jpssantosos5874@gmail.com>
2024-07-02 19:19:51 -0300

    Java Code

--- chess-system/src/chess_pieces/Pawn.java
+++ chess-system/src/chess_pieces/Pawn.java
@@ -32,7 +32,7 @@ public class Pawn extends ChessPiece {
                     && getMoveCount() == 0) {
                 mat[p.getRow()][p.getColumn()] = true;
             }
-            //Left diagonal
+            // Left diagonal
             p.setValues(position.getRow() - 1, position.getColumn() - 1);
             if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
                 mat[p.getRow()][p.getColumn()] = true;
@@ -43,15 +43,14 @@ public class Pawn extends ChessPiece {
                 mat[p.getRow()][p.getColumn()] = true;
             }
 
-        }
-        else {
+        } else {
             p.setValues(position.getRow() + 1, position.getColumn());
             if (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
                 mat[p.getRow()][p.getColumn()] = true;
             }
 
             // 2 line
-            p.setValues(position.getRow() + 2 , position.getColumn());
+            p.setValues(position.getRow() + 2, position.getColumn());
             Position p2 = new Position(position.getRow() + 1, position.getColumn());
             if (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p) && getBoard().positionExists(p2)
                     && !getBoard().thereIsAPiece(p2)
